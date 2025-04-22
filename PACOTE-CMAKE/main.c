4fdce45caeedfce9676b829756c8c54d2f2f2466
Frankl1sales <franklinsales1711@gmail.com>
2025-04-22 16:37:22 -0300

    Adicionado conteúdo EXTRA - Como executar no CMAKE2
    Co-authored-by: NEAGAIA <neagaiacode@gmail.com>

--- PACOTE-CMAKE/main.c
+++ PACOTE-CMAKE/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Olá, mundo! Este é um projeto C com CMake.\n");
+    printf("Olá, mundo! Este é um projeto C com CMake, tudo bom?.\n");
     return 0;
 }
